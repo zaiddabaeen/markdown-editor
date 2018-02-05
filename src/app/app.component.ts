8099bf61b55f1c40f7144f55cf28fd4266ae1b30
Zaid Daba'een <zaid@chargifi.com>
2018-02-05 08:54:55 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as showdown from 'showdown';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements AfterViewInit {
+
+  public inputText: string;
+  public outputText: string;
+  public wordCount: number;
+
+  private converter;
+
+  constructor() {
+    this.converter = new showdown.Converter();
+  }
+
+  ngAfterViewInit(): void {
+    const lastInput = localStorage.getItem('last_input');
+
+    if (lastInput) {
+      this.inputText = lastInput;
+      this.render();
+    }
+  }
+
+  render() {
+    this.outputText = this.converter.makeHtml(this.inputText);
+
+    localStorage.setItem('last_input', this.inputText);
+
+    this.wordCount = this.inputText.trim().split(/\s+/).length;
+  }
 }
